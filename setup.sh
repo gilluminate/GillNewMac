7cf089252f2553dd5064e30c2a2469122a14fef3
Jason Gill <petethechop@gmail.com>
2013-08-13 22:38:59 -0600

    the main show

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+brews=(`cat brewList`)
+casks=(`cat brewCaskList`)
+npms=(`cat npmList`)
+echo "installing homebrew"
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+echo "installing brew packages"
+for package in "${brews[@]}"
+	do
+		brew install $package
+done
+echo "installing cask apps"
+for app in "${casks[@]}"
+	do
+		brew cask install $app
+done
+echo "installing npm packages"
+for package in "${npms[@]}"
+	do
+		npm install -g $package
+done
+echo "opening app store for convenience"
+open -a App\ Store
